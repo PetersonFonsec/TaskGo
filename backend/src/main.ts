2b20261d7e0d0f3b441310ad8ec2ef1ea713b2aa
peterson <petersonsimiao1@gmail.com>
2025-08-20 23:39:48 -0300

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,33 @@
+import otelSDK from "./tracing";
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import helmet from 'helmet';
+// import * as compression from 'compression';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  await otelSDK.start();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  // app.use(compression());
+
+  const config = new DocumentBuilder()
+    .setTitle('TaskGo backend')
+    .setDescription('The TaskGo backend API documentation')
+    .setVersion('1.0')
+    .addTag('TaskGo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
+
 bootstrap();
+ 
